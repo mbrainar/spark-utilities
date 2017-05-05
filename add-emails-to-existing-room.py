d03522ea524db51ba12f10a4694074db94228e59
Matthew Brainard <mbrainar@cisco.com>
2017-05-05 15:18:54 -0400

    initial commit

---
+++ add-emails-to-existing-room.py
@@ -0,0 +1,25 @@
+"""
+This will take a CSV file, parse it for an email field, and add each email to an existing room
+
+"""
+
+from ciscosparkapi import CiscoSparkAPI
+import os
+import csv
+
+_my_spark_token = os.environ.get("MYSPARKTOKEN")
+spark = CiscoSparkAPI(access_token=_my_spark_token)
+
+room_name = ""
+rooms = spark.rooms.list()
+for r in rooms:
+    if r.title == room_name:
+        room_id = r.id
+        break
+
+file = "test.csv"
+
+with open(file) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        spark.memberships.create(room_id, personEmail=row['Email'])
