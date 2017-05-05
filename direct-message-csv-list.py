c18e525b7be54858918fbffe54f20ae4d881c130
Matthew Brainard <mbrainar@cisco.com>
2017-05-05 14:48:05 -0400

    initial commit; parse CSV for emails, send spark messages to each

---
+++ direct-message-csv-list.py
@@ -0,0 +1,19 @@
+"""
+This will take a CSV file, parse it for an email field, and send a message to each email
+
+"""
+
+from ciscosparkapi import CiscoSparkAPI
+import os
+import csv
+
+_my_spark_token = os.environ.get("MYSPARKTOKEN")
+spark = CiscoSparkAPI(access_token=_my_spark_token)
+
+file = "test.csv"
+
+with open(file) as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		message = "Hello {}. This is an automated message.".format(row['First Name'])
+        spark.messages.create(toPersonEmail=row['Email'],markdown=message)
